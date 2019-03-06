c2a055fae5f07408a27178d8494e166d7cedc362
Mike Landers <landers.mike@gmail.com>
2019-03-06 15:46:47 -0800

    MVP

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,51 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from './Controls';
+const mock = jest.fn();
+
+describe('Controls Component', () => {
+	test('matches snapshot', () => {
+		const tree = renderer.create(<Controls />);
+		expect(tree.toJSON()).toMatchSnapshot();
+	});
+
+	test('"Close Gate" fires a function ', () => {
+		const { getByText } = render(
+			<Controls closed={false} locked={false} toggleClosed={mock} />
+		);
+
+		const button = getByText(/close gate/i);
+
+		fireEvent.click(button);
+		expect(mock).toHaveBeenCalled();
+	});
+	test('"Open Gate" fires a function ', () => {
+		const { getByText } = render(<Controls closed={true} locked={false} toggleLocked={mock} />);
+
+		const button = getByText(/open gate/i);
+
+		fireEvent.click(button);
+		expect(mock).toHaveBeenCalled();
+	});
+
+	test('"Lock Gate" fires a function ', () => {
+		const { getByText } = render(<Controls closed={true} locked={false} toggleLocked={mock} />);
+
+		const button = getByText(/lock gate/i);
+
+		fireEvent.click(button);
+		expect(mock).toHaveBeenCalled();
+	});
+	test('"Unlock Gate" fires a function ', () => {
+		const { getByText } = render(<Controls closed={true} locked={true} toggleLocked={mock} />);
+
+		const button = getByText(/unlock gate/i);
+
+		fireEvent.click(button);
+		expect(mock).toHaveBeenCalled();
+	});
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
 import 'react-testing-library/cleanup-after-each';
@@ -7,12 +7,44 @@ import 'react-testing-library/cleanup-after-each';
 import Display from './Display';
 
 describe('Display Component', () => {
-	it('matches snapshot', () => {
+	test('matches snapshot', () => {
 		const tree = renderer.create(<Display />);
 		expect(tree.toJSON()).toMatchSnapshot();
 	});
-	it('should ', () => {
-		render(<Display />);
+	test('should be locked and closed ', () => {
+		const { getByTestId } = render(<Display closed={false} locked={false} />);
+		const lock = getByTestId('lock');
+		const close = getByTestId('close');
+		expect(lock.textContent).toMatch(/unlocked/i);
+		expect(close.textContent).toMatch(/open/i);
+	});
+
+	test('When "locked" use "red-class" ', () => {
+		const { getByText } = render(<Display locked={true} closed={true} />);
+
+		const unlockGate = getByText(/locked/i);
+
+		expect(unlockGate.className).toEqual('led red-led');
+	});
+	test('When "locked" use "green-class" ', () => {
+		const { getByText } = render(<Display locked={false} closed={true} />);
+
+		const unlockGate = getByText(/unlocked/i);
+
+		expect(unlockGate.className).toEqual('led green-led');
+	});
+	test('When "closed" use "red-class" ', () => {
+		const { getByText } = render(<Display locked={true} closed={true} />);
+
+		const unlockGate = getByText(/closed/i);
+
+		expect(unlockGate.className).toEqual('led red-led');
+	});
+	test('When "closed" use "green-class" ', () => {
+		const { getByText } = render(<Display locked={false} closed={false} />);
+
+		const unlockGate = getByText(/open/i);
+
+		expect(unlockGate.className).toEqual('led green-led');
 	});
 });
-// Test away!
