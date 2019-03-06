45ad0452a4ec17b2fca9ed5a59879f3fe5885802
Mike Landers <landers.mike@gmail.com>
2019-03-06 12:27:57 -0800

    setup

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display';
+
+describe('Display Component', () => {
+	it('matches snapshot', () => {
+		const tree = renderer.create(<Display />);
+		expect(tree.toJSON()).toMatchSnapshot();
+	});
+	it('should ', () => {
+		render(<Display />);
+	});
+});
 // Test away!
